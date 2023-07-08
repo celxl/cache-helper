2887e106fdd6affee224a32b3984370a7dda0f88
Fidel Pérez Celoio <fidel.perez@transcom.com>
2023-07-08 18:46:54 +0200

    feat: initialization

---
+++ _test/inmemory_cache.test.ts
@@ -0,0 +1,172 @@
+import { getCacheInstance } from '../src/inmemory-cache';
+const ttl = 5
+
+describe('Class initialization - error handling', () => {
+    it('Should throw error - wrong params cacheEnable type', () => {
+        expect(() => {
+            getCacheInstance({ cacheEnable: "asdf" as unknown as boolean, cacheTtl: 5 });
+        })
+            .toThrow("Type argument exception: options.cacheEnable parameter must be type boolean");
+    });
+
+    it('Should throw error - wrong params cacheTtl type', () => {
+        expect(() => {
+            getCacheInstance({ cacheEnable: true, cacheTtl: '5' as unknown as number });
+        })
+            .toThrow("Type argument exception: options.cacheTtl parameter must be type number");
+    });
+
+});
+
+describe('Class initialization', () => {
+
+    it('Should always return same object', () => {
+        getCacheInstance({ cacheEnable: true, cacheTtl: ttl });
+
+        const cache2 = getCacheInstance({ cacheEnable: true, cacheTtl: (ttl + 20) });
+
+        expect(cache2.cacheTtl).toBe(ttl);
+    });
+
+});
+
+describe('Cache usage', () => {
+    let cache: any;
+    beforeAll(() => {
+        cache = getCacheInstance();
+    });
+
+    it('Should store value in cache', async () => {
+        const 
+            input = 5,
+            output = 5;
+            
+        await cache.set('test1.1', input);
+        const result = await cache.get('test1.1');
+
+        expect(result).toBe(output);
+    });
+
+    it('Should execute callback', async ()=> {
+        const callback = jest.fn(()=> {return 1});
+        
+        const result = await cache.get('test1.2', ()=>{
+            return callback();
+        });
+
+        expect(callback).toHaveBeenCalled();
+        expect(result).toBe(1);
+        
+    });
+
+    it('Should not execute callback', async ()=> {
+        const callback = jest.fn(()=> {return 1});
+        
+        cache.set('test1.3', 5);
+        const result = await cache.get('test1.3', ()=>{
+            return callback();
+        });
+
+        expect(callback).not.toHaveBeenCalled();
+        expect(result).toBe(5);
+    });
+
+    it('Should execute/wait for promise', async ()=> {
+        const callback = jest.fn(async () => {
+            await new Promise(resolve => setTimeout(resolve, 500))
+            return 1
+        });
+        
+        const result = await cache.get('test1.4', ()=>{
+            return callback();
+        });
+
+        expect(callback).toHaveBeenCalled();
+        expect(result).toBe(1);
+        
+    });
+
+    it('Should not execute/wait for promise', async ()=> {
+        const callback = jest.fn(async () => {
+            await new Promise(resolve => setTimeout(resolve, 500))
+            return 1
+        });
+        
+        cache.set('test1.5', 5);
+        const result = await cache.get('test1.5', ()=>{
+            return callback();
+        });
+
+        expect(callback).not.toHaveBeenCalled();
+        expect(result).toBe(5);
+    });
+
+    it('Should get stored keys', async () => {
+        cache.clear();
+        const output = ['hello', 'world', 'happy'];
+
+        output.map( key => cache.set(key, 0) );
+        const result = cache.getKeys();
+
+        expect(result).toEqual(output);
+    });
+
+    it('Should delete stored key-value', async () => {
+        cache.clear();
+        const
+            input = ['hello', 'world', 'happy'],
+            output = input.slice(1);
+
+        input.map( key => cache.set(key, 0) );
+        cache.delete(input[0]);
+
+        const
+            cachedResult = null, 
+            result = cache.getKeys();
+
+        expect(result).toEqual(output);
+        expect(cachedResult).toBeNull();
+    });
+
+    it('Should be expired', async () => {
+        const
+            input = 5,
+            output = null;
+            
+        await cache.set('test', input);
+        const time = cache.cacheTtl;
+        
+        await new Promise(resolve => setTimeout(resolve, time*1000));
+        
+        const result = await cache.get('test');
+        expect(output).toBe(result);
+    },(ttl+5)*1000);
+
+});
+
+describe('Class wrong usage', () => {
+    let cache: any;
+    beforeAll(() => {
+        cache = getCacheInstance()
+    });
+
+    it('Should should trow missing argument error - get', async () => {
+        expect(async ()=>{
+            await cache.get();
+        }).rejects.toThrow('Missing argument exception: key parameter is mandatory');
+    });
+
+    it('Should should trow missing argument error - set', async () => {
+        expect(async ()=>{
+            await cache.set();
+        }).rejects.toThrow('Missing argument exception: key parameter is mandatory');
+    });
+
+    it('Should should trow missing argument error - set', async () => {
+        expect(async ()=>{
+            await cache.delete();
+        }).rejects.toThrow('Missing argument exception: key parameter is mandatory');
+    });
+    
+
+})
---
+++ src/inmemory-cache.ts
@@ -0,0 +1,186 @@
+
+import NodeCache from 'node-cache';
+
+enum ErrorHandlerRuleEnum {
+    MANDATORY = 'mandatory',
+    TYPE = 'data-type'
+}
+
+export interface IInMemoryCacheOptions {
+    /**
+     * @description
+     * When false cache wont be used - useful for debugging proposes
+     */
+    cacheEnable: boolean;
+
+    /**
+     * @description
+     * time in seconds for cache expiration 
+     */
+    cacheTtl: number;
+
+}
+
+interface IErrorHandlerOptions {
+    name: string;
+    value: string | boolean | number;
+    rule: ErrorHandlerRuleEnum,
+    type?: string
+}
+
+export var instance: InMemoryCache;
+
+/**
+ * 
+ */
+class InMemoryCache {
+    private _cache: NodeCache;
+    private _cacheEnable: boolean;
+    private _cacheTtl: number;
+
+    constructor(options: IInMemoryCacheOptions = {cacheEnable: true, cacheTtl: 1}) {
+        this._errorHandler({
+            name: 'options.cacheEnable',
+            rule: ErrorHandlerRuleEnum.TYPE,
+            type: 'boolean',
+            value: options.cacheEnable
+        });
+
+        this._errorHandler({
+            name: 'options.cacheTtl',
+            rule: ErrorHandlerRuleEnum.TYPE,
+            type: 'number',
+            value: options.cacheTtl
+        });
+        
+        this._cacheEnable = options.cacheEnable;
+        this._cacheTtl = options.cacheTtl;
+        this._cache = (this._cacheEnable) ? new NodeCache({ stdTTL: this._cacheTtl }) : null as unknown as NodeCache;
+    }
+
+    public get cacheTtl(): number {
+        return this._cacheTtl;
+    }
+
+    /**
+     * Process the callback argument, if function return its value,
+     * is Promise wait for its completion and return the promise result
+     * @param callback 
+     * @returns Result of callback argument or null
+     */
+    private async _resolveCallback(callback?: ()=> any | Promise<any>): Promise<any> {
+        let result: any = null;
+
+        if (typeof callback == 'function') {
+            result = callback() ;
+        }
+
+        if (callback instanceof Promise) {
+            result = await callback;
+        }
+
+        return result;
+    }
+
+    /**
+     * Get value from cache by its key,
+     * optionally can pass a callback function|promise as argument 
+     * to in case cache is empty resolve callback, set is result to cache and return 
+     * NOTE: callback will run only if no cache for given key
+     * NOTE: callback won't be executed at all if cache exist for key
+     * @param key The cache key
+     * @param callback 
+     * @returns 
+     */
+    async get(key: string, callback?: ()=> any | Promise<any> ) {
+        this._errorHandler({name: 'key', rule: ErrorHandlerRuleEnum.MANDATORY, value: key});
+        
+        if (!this._cache) return this._resolveCallback(callback);
+
+        let result = this._cache.get(key) || null;
+
+        if (!result) {
+            result = await this._resolveCallback(callback);
+            (result) && this.set(key, result);
+        }
+
+        return result;
+    }
+
+    /**
+     * set a new cache key with its value
+     * @param key 
+     * @param value 
+     */
+    set(key: string, value: any ) {
+        this._errorHandler({name: 'key', rule: ErrorHandlerRuleEnum.MANDATORY, value: key});
+
+        if (!this._cache) return;
+
+        this._cache.set(key, value);
+    }
+
+    /**
+     * Delete/clear from cache the given key
+     * @param key 
+     * @returns 
+     */
+    delete(key: string) {
+        this._errorHandler({name: 'key', rule: ErrorHandlerRuleEnum.MANDATORY, value: key});
+        
+        if (!this._cache) return;
+
+        this._cache.del(key);
+    }
+
+    /**
+     * clear the cache, by removing all keys - values
+     */
+    clear() {
+        if (!this._cache) return;
+
+        this._cache.flushAll();
+    }
+
+    /**
+     * Return all keys in use
+     * @returns string []
+     */
+    getKeys(): string[] {
+        if (!this._cache) return [];
+
+        return this._cache.keys();
+    }
+
+    isEnabled(): boolean {
+        return this._cacheEnable;
+    }
+
+
+    /**
+     * 
+     * @param paramName 
+     * @param paramValue 
+     * @param paramType 
+     * @returns 
+     * @todo
+     * Implement a more robust logic, as the library grows
+     */
+    private _errorHandler(options: IErrorHandlerOptions) {
+        if(options.rule == ErrorHandlerRuleEnum.MANDATORY && options.value === undefined)
+            throw new Error(`Missing argument exception: ${options.name} parameter is mandatory`);
+
+        if(options.rule == ErrorHandlerRuleEnum.TYPE && typeof options.value !== options.type)
+            throw new Error(`Type argument exception: ${options.name} parameter must be type ${options.type}`);
+        
+        return;
+    }
+
+}
+
+export function getCacheInstance(options?: IInMemoryCacheOptions): InMemoryCache{
+    if(!instance) 
+        instance = new InMemoryCache(options);
+
+    return instance;
+}
